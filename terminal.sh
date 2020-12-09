046cbd98f62b951c1b6f43ddb801b6a4c7d033e3
nemscep <nemscepanovic@gmail.com>
2020-12-09 17:59:43 +0100

    Modified terminal.sh a bit

--- terminal.sh
+++ terminal.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
+
 # Installing command line tools
-xcode-select --install
+# xcode-select --install
+
 # Installing zsh
 brew install zsh
 echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # Download Git Auto-Completion
+echo "Installing git auto-completion"
 curl "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash" > $HOME/.git-completion.bash
 
 # Installing git
 echo "Installing Git"
 brew install git
+
+# Installing zsh-git-prompt
+echo "Installing zsh-git-prompt"
 brew install zsh-git-prompt
