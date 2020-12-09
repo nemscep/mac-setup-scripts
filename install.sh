1b614c417a216b001e1818000b1fba41a7435469
nemscep <nemscepanovic@gmail.com>
2020-12-09 23:11:24 +0100

    Added explicit permission granting

--- install.sh
+++ install.sh
@@ -1,36 +1,43 @@
 #!/usr/bin/env bash
+
 # Terminal setup
 echo "Terminal setup..."
+chmod u+x terminal.sh
 ./terminal.sh
 echo "...done"
 echo "############"
 
 # symlinks
 echo "Creating symlinks..."
+chmod u+x symlinks.sh
 ./symlinks.sh
 echo "...done"
 echo "############"
 
 # Install macOS apps
 echo "Installing default apps..."
+chmod u+x ./apps/default-apps.sh
 ./apps/default-apps.sh
 echo "...done"
 echo "############"
 
 # Install default work apps
 echo "Installing default work apps..."
+chmod u+x ./default-work.sh
 ./apps/default-work.sh
 echo "...done"
 echo "############"
 
 # Install android speciifc apps (android-studio, java, etc.)
 echo "Installing android specific apps..."
+chmod u+x ./apps/android.sh
 ./apps/android.sh
 echo "...done"
 echo "############"
 
 # Install necessary drivers
 echo "Installing drivers..."
+chmod u+x drivers.sh
 ./drivers.sh
 echo "...done"
 echo "############"
