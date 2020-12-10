7d1c01d8eed799b5b4bc9b3f2336a985ef20080d
nemscep <nemscepanovic@gmail.com>
2020-12-10 16:17:38 +0100

    Reverted commenting of install.sh

--- install.sh
+++ install.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
 # Terminal setup
-# echo "Terminal setup..."
-# chmod u+x terminal.sh
-# ./terminal.sh
-# echo "...done"
-# echo "############"
+echo "Terminal setup..."
+chmod u+x terminal.sh
+./terminal.sh
+echo "...done"
+echo "############"
 
 # symlinks
-# echo "Creating symlinks..."
-# chmod u+x symlinks.sh
-# ./symlinks.sh
-# echo "...done"
-# echo "############"
+echo "Creating symlinks..."
+chmod u+x symlinks.sh
+./symlinks.sh
+echo "...done"
+echo "############"
 
 # Install macOS apps
-# echo "Installing default apps..."
-# chmod u+x ./apps/default-apps.sh
-# ./apps/default-apps.sh
-# echo "...done"
-# echo "############"
+echo "Installing default apps..."
+chmod u+x ./apps/default-apps.sh
+./apps/default-apps.sh
+echo "...done"
+echo "############"
 
 # Install default work apps
-# echo "Installing default work apps..."
-# chmod u+x ./apps/default-work.sh
-# ./apps/default-work.sh
-# echo "...done"
-# echo "############"
+echo "Installing default work apps..."
+chmod u+x ./apps/default-work.sh
+./apps/default-work.sh
+echo "...done"
+echo "############"
 
 # Install android speciifc apps (android-studio, java, etc.)
 echo "Installing android specific apps..."
@@ -36,8 +36,8 @@ echo "...done"
 echo "############"
 
 # Install necessary drivers
-# echo "Installing drivers..."
-# chmod u+x drivers.sh
-# ./drivers.sh
-# echo "...done"
-# echo "############"
+echo "Installing drivers..."
+chmod u+x drivers.sh
+./drivers.sh
+echo "...done"
+echo "############"
