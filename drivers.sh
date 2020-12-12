79d1a0cd80950cbf247bcfdcd071957c1bb837d5
nemscep <nemscepanovic@gmail.com>
2020-12-12 21:31:45 +0100

    Introduced karabiner-elements into drivers setup

--- drivers.sh
+++ drivers.sh
@@ -6,3 +6,11 @@ brew tap homebrew/cask-drivers
 echo "Installing logitech-options"
 brew cask install logitech-options
 echo "...done"
+
+# Karabiner-Elements, software for intercepting/mapping keyboard keys
+# This is useful when mac has a non-us layout and you'd want some special key-mapping behavior
+# In my particular example, I want to change non_us_backslash to behave as a us-backslash-and-tilda (grave_accent_and_tilde)
+# 
+echo "Installing karabiner-elements"
+brew cask install karabiner-elements
+echo "...done"
